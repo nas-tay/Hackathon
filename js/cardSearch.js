73a317980139d8af5d2428ae57d843ce7f6e534b
Polina <sunnyfur9@gmail.com>
2022-05-07 19:13:07 +0500

    add code

--- js/cardSearch.js
+++ js/cardSearch.js
@@ -1,18 +1,45 @@
-const CardHtml = () => {
+arr = GenerateJSON();
+const CardHtml = (people) => {
   const list = document.querySelector(".card_list");
-  list.appendChild(createElemDOM("div", "d-block w-100 card_carous"));
+
+  return createElemDOM("div", "d-block w-100 card_carous", people.name);
 };
 
 const CardSearch = () => {
   const searchText = document.querySelector("#idCardKey").value;
-  const arrCards = document.querySelectorAll(".carousel-item");
-  arrCards.innerHtml = "";
+  const list = document.querySelector(".card_list");
+  list.innerHTML = "";
+  if (searchText) {
+    arr.forEach((element) => {
+      const search = new RegExp(searchText, "gi");
+      const rez = search.test(element.keyWords);
 
-  CardHtml();
+      if (rez) {
+        list.appendChild(CardHtml(element));
+      }
+    });
+  }
+};
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+}
+
+const CardRandom = () => {
+  const list = document.querySelector(".card_list");
+  list.innerHTML = "";
+  const rand = getRandomIntInclusive(1, arr.length);
+
+  list.appendChild(CardHtml(arr[rand - 1]));
 };
 
 document.addEventListener("DOMContentLoaded", () => {
   document
     .querySelector("#idCardFind")
     .addEventListener("click", () => CardSearch());
+  document
+    .querySelector("#idCardRnd")
+    .addEventListener("click", () => CardRandom());
 });
--- js/classPeople.js
+++ js/classPeople.js
@@ -1,1 +1,39 @@
-class People {}
+class People {
+  static _count = 0;
+  constructor(name, photo, about, keyWords) {
+    this.name = name;
+    this.photo = photo;
+    this.about = about;
+    this.keyWords = keyWords;
+    this.id = ++People._count;
+  }
+}
+
+const GenerateJSON = () => {
+  const arrPeople = [];
+  arrPeople.push(
+    new People(
+      "Саша Скочиленко",
+      "Фото",
+      "Жила в Санкт-Петербурге, занималась иллюстрацией.",
+      "музыка, социология"
+    )
+  );
+  arrPeople.push(
+    new People(
+      "Мария Пономаренко",
+      "Фото",
+      "Родом из Барнаула, работала журналисткой.",
+      "литература, экология, урбанистика "
+    )
+  );
+  arrPeople.push(
+    new People(
+      "Галина Довгополая",
+      "Фото",
+      "Пенсионерка. В колонии увлеклась вязанием — вяжет детские вещи для дома матери и ребенка.",
+      ""
+    )
+  );
+  return arrPeople;
+};
